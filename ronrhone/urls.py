776dd4d87cf80246f32bc52d0fde50f0b14845fd
cperreaut <perreaut.clementine@gmail.com>
2021-02-18 21:31:36 +0100

    Redirection url

--- ronrhone/urls.py
+++ ronrhone/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="accueil")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/ronRhonE1901', admin.site.urls),
     path("ronrhone/", include("gestion_association.urls")),
