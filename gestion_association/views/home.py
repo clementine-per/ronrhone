efa898427fa7617a85e5403d639b16e49617540f
cperreaut <perreaut.clementine@gmail.com>
2021-05-06 13:18:51 +0200

    TDB partie bons de stérilisation

--- gestion_association/views/home.py
+++ gestion_association/views/home.py
@@ -1,10 +1,13 @@
 import sys
+from datetime import timedelta
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
+from django.utils import timezone
 
-from gestion_association.models.adoption import TarifAdoption, TarifBonSterilisation, Adoption
+from gestion_association.models import OuiNonChoice
+from gestion_association.models.adoption import TarifAdoption, TarifBonSterilisation, Adoption, BonSterilisation
 from gestion_association.models.animal import Animal, statuts_association
 from gestion_association.models.famille import Famille
 
@@ -14,6 +17,12 @@ def index(request):
     selected = "accueil"
     title = "Tableau de bord"
 
+    today = timezone.now().date()
+    interval_10 = timezone.now().date() + timedelta(days=10)
+    # Valeurs str utilisées dans le template html
+    today_str = today.strftime("%Y-%m-%d")
+    interval_10_str = interval_10.strftime("%Y-%m-%d")
+
     statuts_association_filter = ""
     for statut in statuts_association:
         statuts_association_filter += 'statuts='
@@ -21,14 +30,17 @@ def index(request):
         statuts_association_filter += '&'
     # Partie adoptions
     # Adoptions attendant leur visite de contrôle
-    adoption_adoptes = Adoption.objects.filter(pre_visite='OUI').filter(visite_controle='NON').count()
+    adoption_adoptes = Adoption.objects.filter(pre_visite=OuiNonChoice.OUI.name)\
+        .filter(visite_controle=OuiNonChoice.NON.name).count()
     # Adoptions à clore
-    adoption_over = Adoption.objects.filter(animal__statut='ADOPTE').filter(visite_controle='OUI').count()
+    adoption_over = Adoption.objects.filter(animal__statut='ADOPTE')\
+        .filter(visite_controle=OuiNonChoice.OUI.name).count()
     # Adoptions pré-visites
-    adoption_previsite = Adoption.objects.filter(animal__statut='ADOPTION').filter(pre_visite='NON').count()
+    adoption_previsite = Adoption.objects.filter(animal__statut='ADOPTION')\
+        .filter(pre_visite=OuiNonChoice.NON.name).count()
     # Adoptions en cours (à payer, ou à contrôler)
-    adoption_en_cours = Adoption.objects.filter(animal__statut='ADOPTION').filter(pre_visite='OUI')\
-        .exclude(pre_visite='NON').count()
+    adoption_en_cours = Adoption.objects.filter(animal__statut='ADOPTION').filter(pre_visite=OuiNonChoice.OUI.name)\
+        .exclude(pre_visite=OuiNonChoice.NON.name).count()
     #A l'adoption
     a_l_adoption = Animal.objects.filter(statut='A_ADOPTER').count()
     # A proposer à l'adoption
@@ -36,11 +48,16 @@ def index(request):
 
     # Partie soins
     # Animaux à stériliser
-    sterilises = Animal.objects.filter(sterilise='NON').filter(statut__in=statuts_association).count()
+    sterilises = Animal.objects.filter(sterilise=OuiNonChoice.NON.name).filter(statut__in=statuts_association).count()
     # Animaux en soin
     soins = Animal.objects.filter(statut='SOIN').count()
     #Animaux à tester (fiv/felv)
     fiv_felv = Animal.objects.filter(statut__in=statuts_association).filter(Q(fiv='NT')|Q(felv='NT')).count()
+    # Bon de stérilisation à envoyer
+    bon_a_envoyer = BonSterilisation.objects.filter(envoye=OuiNonChoice.NON.name).count()
+    # Bon de stérilisation arrivant à expiation (10 jours)
+    bon_a_utilise = BonSterilisation.objects.filter(utilise=OuiNonChoice.NON.name).filter(date_max__gte=today)\
+        .filter(date_max__lte=interval_10).count()
 
     # Partie FA
     # Animaux en FA
