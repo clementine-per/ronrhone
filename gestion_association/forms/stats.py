8b9daefacad891b49eb27d7f3cfc3a7eddbbc0bd
Clémentine Perreaut <clementineperreaut@airdeclementine.home>
2025-06-28 15:24:18 +0200

    Stats enhancements

---
+++ gestion_association/forms/stats.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from django.forms import ChoiceField, Form, Select
+from django.db.models import BLANK_CHOICE_DASH
+
+class AnneeChoice(Enum):
+    ANNEE_2020 = "2020"
+    ANNEE_2021 = "2021"
+    ANNEE_2022 = "2022"
+    ANNEE_2023 = "2023"
+    ANNEE_2024 = "2024"
+    ANNEE_2025 = "2025"
+    ANNEE_2026 = "2026"
+    ANNEE_2027 = "2027"
+    ANNEE_2028 = "2028"
+    ANNEE_2029 = "2029"
+    ANNEE_2030 = "2030" 
+
+
+class AgeChoice(Enum):
+    CHATON = "Chaton (0-6 mois)"
+    ADULTE = "Adulte (6 mois - 8 ans)"
+    SENIOR = "Senior (8 ans et plus)"
+
+
+class DureeAdoptionStatsForm(Form):
+    annee = ChoiceField(
+        choices=BLANK_CHOICE_DASH + [(tag.value, tag.value) for tag in AnneeChoice],
+        widget=Select(),
+        required=False,
+    )
+    age = ChoiceField(
+        choices=BLANK_CHOICE_DASH + [(tag.name, tag.value) for tag in AgeChoice],
+        widget=Select(),
+        required=False,
+    )
--- gestion_association/views/stats.py
+++ gestion_association/views/stats.py
@@ -6,7 +6,10 @@ from django.shortcuts import render
 
 from django.utils.timezone import datetime
 
+from gestion_association.forms.stats import DureeAdoptionStatsForm
 from gestion_association.models.adoption import Adoption
+from django.db.models import F, IntegerField, ExpressionWrapper, Avg
+from django.db.models.functions import ExtractYear, ExtractMonth
 
 
 @login_required
@@ -29,9 +32,39 @@ def index(request):
 
     while (i < 13):
         labels_mois.append(calendar.month_name[i])
-        data_adoptions_current.append(adoptions.filter(date__year=date.year).filter(date__month=i).count())
-        data_adoptions_past.append(adoptions.filter(date__year=date.year - 1).filter(date__month=i).count())
+        data_adoptions_current.append(adoptions.filter(date__year=current).filter(date__month=i).count())
+        data_adoptions_past.append(adoptions.filter(date__year=past).filter(date__month=i).count())
         i += 1
+
+    # Prise en compte des filtres utilisateurs éventuels
+    if request.method == "POST":
+        adoption_duree_form = DureeAdoptionStatsForm(request.POST)
+        if adoption_duree_form.is_valid():
+            annee = adoption_duree_form.cleaned_data.get("annee")
+            age = adoption_duree_form.cleaned_data.get("age")
+            if annee:
+                adoptions = adoptions.filter(date__year=annee)
+            if age:
+                adoptions = adoptions.annotate(start_year=ExtractYear('animal__date_naissance'),
+                                   start_month=ExtractMonth('animal__date_naissance'),
+                                   end_year=ExtractYear('date'),
+                                   end_month=ExtractMonth('date'),
+                ).annotate(
+                    month_diff=ExpressionWrapper(
+                        (F('end_year') - F('start_year')) * 12 + (F('end_month') - F('start_month')),
+                        output_field=IntegerField()
+                    )
+                )
+                if age == "CHATON":
+                    adoptions = adoptions.filter(month_diff__lt=6)
+                elif age == "ADULTE":
+                    adoptions = adoptions.filter(month_diff__gte=6).filter(month_diff__lt=96)
+                elif age == "SENIOR":
+                    adoptions = adoptions.filter(month_diff__gte=96)
+
+    else:
+        adoption_duree_form = DureeAdoptionStatsForm()
+
 
     # Partie Adoptions par durée
     labels_durees = ["Moins de 2 mois", "2 à 4 mois", "4 à 6 mois", "Plus de 6 mois"]
@@ -41,6 +74,9 @@ def index(request):
     data_adoptions_duree.append(adoptions.filter(nb_jours__gte=60).filter(nb_jours__lt=120).count())
     data_adoptions_duree.append(adoptions.filter(nb_jours__gte=120).filter(nb_jours__lt=180).count())
     data_adoptions_duree.append(adoptions.filter(nb_jours__gte=180).count())
+
+    moyenne = adoptions.aggregate(moyenne=Avg('nb_jours'))
+    total_adoptions = adoptions.count()
 
 
     return render(request, "gestion_association/stats.html", locals())
